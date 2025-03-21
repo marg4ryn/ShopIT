698e2d725f9ad5a98cd02174c9dc6ba7cbc0b177
marg4ryn <poweski.jan@gmail.com>
2025-03-21 19:34:40 +0100

    sidebar fix

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -35,7 +35,7 @@ export default function Sidebar() {
 
   return (
     <aside className="min-h-screen">
-      <nav className={`h-full flex flex-col items-center border-0 overflow-hidden ${expanded ? 'bg-neutral-800' : 'bg-neutral-900'}`}>
+      <nav className={`h-full flex flex-col items-center border-0 transition-all duration-300 overflow-hidden ${expanded ? 'bg-neutral-800' : 'bg-neutral-900'}`}>
         
         <div className="p-4 pb-2 flex">
           <p className={`overflow-hidden transition-all text-xl font-bold text-center text-white ${expanded ? "w-64" : "w-0" }`}>SORTING</p>
@@ -58,7 +58,7 @@ export default function Sidebar() {
           </button>
 
             {isOpen && (
-              <ul className={`flex flex-col ${expanded ? "w-64 opacity-100 visible" : "w-0 opacity-0 invisible"} transition-opacity duration-300 border-0 mt-1 bg-white rounded overflow-hidden`}>
+              <ul className={`flex flex-col ${expanded ? "opacity-100 visible" : "opacity-0 invisible"} transition-all duration-300 border-0 mt-1 bg-white rounded overflow-hidden`}>
                 {options.map((option, index) => (
                   <li
                     key={index}
@@ -66,7 +66,7 @@ export default function Sidebar() {
                       setSelected(option);
                       setIsOpen(false);
                     }}
-                    className={`px-4 py-2 hover:bg-gray-100 cursor-pointer`}
+                    className={`${expanded ? "opacity-100 visible" : "opacity-0 invisible"} transition-all duration-300 px-4 py-2 hover:bg-gray-100 cursor-pointer`}
                   >
                     {option}
                   </li>
@@ -76,25 +76,25 @@ export default function Sidebar() {
           </div>
         </div>
 
-        <div className="p-4 pb-2 flex justify-between items-center">
-          <p className={`overflow-hidden transition-all text-xl font-bold text-center text-white ${expanded ? "w-64" : "w-0" } ${isOpen ? "mt-2" : "mt-0"}`}>FILTERING</p>
+        <div className="p-4">
+          <p className={`overflow-hidden text-xl font-bold text-center text-white ${expanded ? "w-64" : "w-0" } ${isOpen ? "mt-2" : "mt-0"}`}>FILTERING</p>
         </div>
 
-        <div className={`p-4 pb-2 flex overflow-hidden justify-center items-center transition-all ${expanded ? "w-64" : "w-0"}`}>
+        <div className={`p-4 pb-2 flex overflow-hidden justify-center items-center transition-all duration-300 ${expanded ? "w-64" : "w-0"}`}>
           {expanded && (
             <div className="text-center w-64">
               <h2 className="font-bold text-white">Price:</h2>
               <div className="flex justify-center gap-4 mt-2">
                 <input
-                  type="text"
+                  type="number"
                   placeholder="From"
-                  className={`px-4 py-2 bg-white w-30 border rounded-md focus:outline-none focus:ring-3 focus:ring-black
+                  className={`w-full px-4 py-2 bg-white text-black rounded-md flex justify-between items-center focus:outline-none focus:ring-3 focus:ring-black
                     transition-opacity duration-300 ${expanded ? "opacity-100 visible" : "opacity-0 invisible"}`}
                 />
                 <input
-                  type="text"
+                  type="number"
                   placeholder="To"
-                  className={`px-4 py-2 bg-white w-30 border rounded-md focus:outline-none focus:ring-3 focus:ring-black
+                  className={`w-full px-4 py-2 bg-white text-black rounded-md flex justify-between items-center focus:outline-none focus:ring-3 focus:ring-black
                     transition-opacity duration-300 ${expanded ? "opacity-100 visible" : "opacity-0 invisible"}`}
                 />
               </div>
@@ -102,30 +102,35 @@ export default function Sidebar() {
           )}
         </div>
 
-        <div className={`p-4 pb-2 flex transition-all ${expanded ? "w-64 opacity-100" : "w-0 opacity-0 overflow-hidden"}`}>
+        <div className={`p-4 pb-2 flex transition-all duration-300 ${expanded ? "w-64 opacity-100" : "w-0 opacity-0 overflow-hidden"}`}>
           {expanded && (
             <div className="text-center w-64">
               <h2 className="font-bold text-white">Categories:</h2>
-              <ul className={`space-y-2`}>
+              <ul className={`space-y-2 mt-2`}>
                 {categories.map((category) => (
-                  <li key={category._id} className="flex justify-between items-center p-2 hover:bg-gray-700">
-                  <span className="text-white text-semibold">{category.name}</span>
-                  <input
-                    type="checkbox"
-                    checked={selectedCategories.includes(category.name)}
-                    onChange={() => toggleCategory(category.name)}
-                  />
-                </li>
+                  <li 
+                    key={category._id} 
+                    className="flex justify-between items-center p-2 hover:bg-gray-700 cursor-pointer"
+                    onClick={() => toggleCategory(category.name)}
+                  >
+                    <span className="text-white font-semibold">{category.name}</span>
+                    <input
+                      type="checkbox"
+                      checked={selectedCategories.includes(category.name)}
+                      onChange={(e) => e.stopPropagation()}
+                      className="cursor-pointer"
+                    />
+                  </li>
                 ))}
               </ul>
             </div>
           )}
         </div>
 
-        <div className="w-fit p-4 pb-2">
+        <div className={`w-fit p-4 pb-2 flex transition-all duration-300 ${expanded ? "max-w-64 opacity-100" : "max-w-0 opacity-0"}`}>
           <button
-            className={`flex bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded transition-opacity ease-in-out 
-              ${expanded ? "opacity-100" : "opacity-0"}`}
+            className={`flex bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded transition-all duration-300
+              ${expanded ? "max-w-32 opacity-100" : "max-w-0 opacity-0"}`}
           >
             Apply filters
           </button>
